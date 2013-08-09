042460925041c1b53bc8afa5cfd89ea8ba86b412
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-09 17:59:54 +0400

    y works

--- js/relativepositioning.js
+++ js/relativepositioning.js
@@ -1,35 +1,31 @@
 function relativePositioning() {
-var elements = document.querySelectorAll(".step"); 
-for (var i = 0; i < elements.length; i++) {
-   var x = elements[i].dataset.x;
-        if (x.substring(0,1) === '+') {
-           var lastStep = elements[i-1].dataset.x;      
-           var position = parseInt(lastStep) + parseInt(x);
-           elements[i].dataset.x = position;      
+    var elements = document.querySelectorAll(".step");
+    for (var i = 0; i < elements.length; i++) {
+        var x = elements[i].dataset.x;
+        if (x.substring(0, 1) === '+') {
+            var lastStep = elements[i - 1].dataset.x;
+            var position = parseInt(lastStep) + parseInt(x);
+            elements[i].dataset.x = position;
+        } else if (x.substring(0, 1) === '-') {
+            var lastStep = elements[i - 1].dataset.x;
+            var position = parseInt(lastStep) + parseInt(x);
+            elements[i].dataset.x = position;
+        } else {
+            elements[i].dataset.x = x;
         }
-        else if (x.substring(0,0) === '-') {
-           var lastStep = elements[i-1].dataset.x;
-           var position = parseInt(lastStep) - parseInt(x);
-           elements[i].dataset.x = position; 
+    }
+    for (var i = 0; i < elements.length; i++) {
+        var y = elements[i].dataset.y;
+        if (y.substring(0, 1) === '+') {
+            var lastStep = elements[i - 1].dataset.y;
+            var position = parseInt(lastStep) + parseInt(y);
+            elements[i].dataset.y = position;
+        } else if (y.substring(0, 1) === '-') {
+            var lastStep = elements[i - 1].dataset.y;
+            var position = parseInt(lastStep) + parseInt(y);
+            elements[i].dataset.y = position;
+        } else {
+            elements[i].dataset.y = y;
         }
-        else {     
-           elements[i].dataset.x = x; 
-        } 
-   }   
- // for (var i = 0; i < elements.length; i++) {
- //  var y = elements[i].dataset.y;
- //      if (y.substring(0,1) === '+') {
- //         var lastStep = elements[i-1].dataset.y;
- //         var position = parseInt(lastStep) + parseInt(y);
- //         elements[i].dataset.y = position;         
- //      }
- //      else if (y.substring(0,0) === '-') {
- //         var lastStep = elements[i-1].dataset.y;
- //         var position = parseInt(lastStep) - parseInt(y);
- //         elements[i].dataset.y = position; 
- //      }
- //      else {     
- //         elements[i].dataset.y = y; 
- //      }
- //    }       
+    }
 }
