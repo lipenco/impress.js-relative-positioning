32a8a031907f9869cbb7bb6aaea895bf6ad9aed1
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-15 14:34:45 +0400

    error fixed - sanity check works

--- js/relativepositioning.js
+++ js/relativepositioning.js
@@ -5,48 +5,48 @@ function relativePositioning() {
         var x = elements[i].dataset.x;
         var y = elements[i].dataset.y;
         var z = elements[i].dataset.z;
-        if (elements[i].dataset.x == undefined) {
+        if (elements[i].dataset.x === undefined) {
             x = "0";
         }
         if (x.substring(0, 2) === '+=') {
             var lastStep = elements[i - 1].dataset.x;
-            var position = parseInt(lastStep) + parseInt(x);
+            var position = parseInt(lastStep) + parseInt(x.substring(2,x.length));
             elements[i].dataset.x = position;
         } else if (x.substring(0, 2) === '-=') {
             var lastStep = elements[i - 1].dataset.x;
-            var position = parseInt(lastStep) + parseInt(x);
+            var position = parseInt(lastStep) - parseInt(x.substring(2,x.length));
             elements[i].dataset.x = position;
         } else {
             elements[i].dataset.x = x;
         }
 
-        if (elements[i].dataset.y == undefined) {
+        if (elements[i].dataset.y === undefined) {
             y = "0";
         }
 
         if (y.substring(0, 2) === '+=') {
             var lastStep = elements[i - 1].dataset.y;
-            var position = parseInt(lastStep) + parseInt(y);
+            var position = parseInt(lastStep) + parseInt(y.substring(2,y.length));
             elements[i].dataset.y = position;
         } else if (y.substring(0, 2) === '-=') {
             var lastStep = elements[i - 1].dataset.y;
-            var position = parseInt(lastStep) + parseInt(y);
+            var position = parseInt(lastStep) - parseInt(y.substring(2,y.length));
             elements[i].dataset.y = position;
         } else {
             elements[i].dataset.y = y;
         }
 
-        if (elements[i].dataset.z == undefined) {
+        if (elements[i].dataset.z === undefined) {
             z = "0";
         }
 
         if (z.substring(0, 2) === '+=') {
             var lastStep = elements[i - 1].dataset.z;
-            var position = parseInt(lastStep) + parseInt(z);
+            var position = parseInt(lastStep) + parseInt(z.substring(2,z.length));
             elements[i].dataset.z = position;
         } else if (z.substring(0, 2) === '-=') {
             var lastStep = elements[i - 1].dataset.z;
-            var position = parseInt(lastStep) + parseInt(z);
+            var position = parseInt(lastStep) - parseInt(z.substring(2,z.length));
             elements[i].dataset.z = position;
         } else {
             elements[i].dataset.z = z;
