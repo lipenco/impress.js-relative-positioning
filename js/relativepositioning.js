160c64e03b64132c84785e20703a7b74de8afa5e
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-15 13:48:35 +0400

    error fixing +=

--- js/relativepositioning.js
+++ js/relativepositioning.js
@@ -8,11 +8,11 @@ function relativePositioning() {
         if (elements[i].dataset.x == undefined) {
             x = "0";
         }
-        if (x.substring(0, 1) === '+') {
+        if (x.substring(0, 2) === '+=') {
             var lastStep = elements[i - 1].dataset.x;
             var position = parseInt(lastStep) + parseInt(x);
             elements[i].dataset.x = position;
-        } else if (x.substring(0, 1) === '-') {
+        } else if (x.substring(0, 2) === '-=') {
             var lastStep = elements[i - 1].dataset.x;
             var position = parseInt(lastStep) + parseInt(x);
             elements[i].dataset.x = position;
@@ -24,11 +24,11 @@ function relativePositioning() {
             y = "0";
         }
 
-        if (y.substring(0, 1) === '+') {
+        if (y.substring(0, 2) === '+=') {
             var lastStep = elements[i - 1].dataset.y;
             var position = parseInt(lastStep) + parseInt(y);
             elements[i].dataset.y = position;
-        } else if (y.substring(0, 1) === '-') {
+        } else if (y.substring(0, 2) === '-=') {
             var lastStep = elements[i - 1].dataset.y;
             var position = parseInt(lastStep) + parseInt(y);
             elements[i].dataset.y = position;
@@ -40,11 +40,11 @@ function relativePositioning() {
             z = "0";
         }
 
-        if (z.substring(0, 1) === '+') {
+        if (z.substring(0, 2) === '+=') {
             var lastStep = elements[i - 1].dataset.z;
             var position = parseInt(lastStep) + parseInt(z);
             elements[i].dataset.z = position;
-        } else if (z.substring(0, 1) === '-') {
+        } else if (z.substring(0, 2) === '-=') {
             var lastStep = elements[i - 1].dataset.z;
             var position = parseInt(lastStep) + parseInt(z);
             elements[i].dataset.z = position;
